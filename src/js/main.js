c14c31fcc9d3703047dfcb37334bdedbedf2953e
Ramil Yarullin <lucky_lucky_2020@list.ru>
2021-03-11 19:59:58 +0300

    адаптив для header and footer

--- src/js/main.js
+++ src/js/main.js
@@ -38,32 +38,6 @@ import './guest-counter.js'
 import './list-checkbox.js'
 import './calendar.js'
 
-import "../../node_modules/swiper/swiper-bundle"
-import Swiper from 'swiper';
-import SwiperCore, { Navigation, Pagination } from 'swiper/core';
-SwiperCore.use([Navigation, Pagination]);
-var swiper = new Swiper('.swiper-container', {
-	slidesPerView: 1,
-	spaceBetween: 0,
-	loop: true,
-	pagination: {
-		el: '.swiper-pagination',
-		type: 'bullets',
-		clickable: true,
-	},
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
-});
-var swiper2 = new Swiper('.swiper-container2', {
-	slidesPerView: 1,
-	spaceBetween: 0,
-	loop: true,
-	pagination: {
-		el: '.swiper-pagination2',
-		type: 'bullets',
-		clickable: true,
-	},
-});
-
+// import "../../node_modules/swiper/swiper-bundle"
+import './swiper.js'
+import './burger.js'
