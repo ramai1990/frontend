66f1717cb25453f7f5bf250a0a4ffde83c258a73
Ramil <ram_ram_14@list.ru>
2021-09-05 17:05:09 +0300

    убрал табуляцию

--- src/assets/mail.php
+++ src/assets/mail.php
@@ -2,46 +2,45 @@
 
 $method = $_SERVER['REQUEST_METHOD'];
 
-//Script Foreach
 $c = true;
 if ( $method === 'POST' ) {
 
-	$project_name = 'new_project';
-	$admin_email  = 'maxgraph23@gmail.com';
-	$form_subject = 'new_theme';
+  $project_name = 'new_project';
+  $admin_email  = 'maxgraph23@gmail.com';
+  $form_subject = 'new_theme';
 
-	foreach ( $_POST as $key => $value ) {
-		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
-			$message .= "
-			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-			</tr>
-			";
-		}
-	}
+  foreach ( $_POST as $key => $value ) {
+    if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
+      $message .= "
+      " . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
+        <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+        <td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
+      </tr>
+      ";
+    }
+  }
 } else if ( $method === 'GET' ) {
 
-	$project_name = 'new_project';
-	$admin_email  = 'maxgraph23@gmail.com';
-	$form_subject = 'new_theme';
+  $project_name = 'new_project';
+  $admin_email  = 'maxgraph23@gmail.com';
+  $form_subject = 'new_theme';
 
-	foreach ( $_GET as $key => $value ) {
-		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
-			$message .= "
-			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-			</tr>
-			";
-		}
-	}
+  foreach ( $_GET as $key => $value ) {
+    if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
+      $message .= "
+      " . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
+        <td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+        <td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
+      </tr>
+      ";
+    }
+  }
 }
 
 $message = "<table style='width: 100%;'>$message</table>";
 
 function adopt($text) {
-	return '=?UTF-8?B?'.Base64_encode($text).'?=';
+  return '=?UTF-8?B?'.Base64_encode($text).'?=';
 }
 
 $headers = "MIME-Version: 1.0" . PHP_EOL .
