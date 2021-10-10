9d184178b0282d563046ec5739fea709a3c5bd88
Ramil <ram_ram_14@list.ru>
2021-10-10 17:09:02 +0300

    updated air-datepicker

--- src/components/calendare/calendar.js
+++ src/components/calendare/calendar.js
@@ -1,97 +1,99 @@
 $(function () {
-  $(".dropdown-date__range_from").datepicker({
+  $(".filter-date").datepicker({
+    onShow: function (dp, animationCompleted) {
+      if (!animationCompleted) {
+        $(".air-datepicker").addClass("active");
+      }
+    },
+    onHide: function (dp, animationCompleted) {
+      if (!animationCompleted) {
+        $(".air-datepicker").removeClass("active");
+      }
+    },
     minDate: new Date(),
     range: true,
-    offset: 10,
-    multipleDatesSeparator: "-",
-    classes: "dropdown-date",
+    multipleDatesSeparator: " - ",
     clearButton: true,
-    prevHtml: `<div class="arrow_prew"></div>`,
-    nextHtml: `<div class="arrow_next"></div>`,
     todayButton: true,
-    navTitles: {
-      days: "<h2>MM yyyy</h2>",
-    },
     language: {
       today: "Применить",
     },
-    onRenderCell: function (date, cellType) {
-      if (cellType == "day") {
-        return {
-          html: `${date.getDate()}<div class="bg-selected"><span></span></div>`,
-        };
-      }
-    },
-    onSelect: function (fd, d, picker) {
-      $(".dropdown-date__range_from").val(fd.split("-")[0]);
-      $(".dropdown-date__range_to").val(fd.split("-")[1]);
-    },
   });
-  $(".datepicker")
-    .find(".datepicker--button")
-    .click(function () {
-      if (this.dataset.action === "today") {
-        $(this)
-          .parents(".dropdown-date")
-          .hide();
-      }
-    });
-  $(".dropdown-date__range_to").click(function () {
-    $(".datepicker").show()
-    $(this)
-      .parents(".dropdown-date")
-      .find(".dropdown-date__range_from")
-      .data("datepicker")
-      .show();
+  $(".datepicker--cells").click(function () {
+    $("[data-action=clear]").addClass("open");
   });
-  $(".dropdown-date__range_from").click(function () {
-    $(".datepicker").show()
-  });
-
-  $('.datepicker-here').datepicker({
-    minDate: new Date(),
-    offset: 10,
-    multipleDatesSeparator: "-",
-    classes: "dropdown-date",
-    clearButton: true,
-    prevHtml: `<div class="arrow_prew"></div>`,
-    nextHtml: `<div class="arrow_next"></div>`,
-    todayButton: true,
-    navTitles: {
-      days: "<h2>MM yyyy</h2>",
-    },
-    language: {
-      today: "Применить",
-    },
-    onRenderCell: function (date, cellType) {
-      if (cellType == "day") {
-        return {
-          html: `${date.getDate()}<div class="bg-selected"><span></span></div>`,
-        };
-      }
-    }
-
-  });
-  $(".datepicker")
-    .find(".datepicker--button")
-    .click(function () {
-      if (this.dataset.action === "today") {
-        $(this)
-          .parents(".dropdown-date")
-          .hide();
-      }
-    });
-  $(".datepicker-here").click(function () {
-    $(".datepicker").show()
-  });
-
-  $('.datepicker--cells').click(function () {
-    $("[data-action=clear]").addClass('open');
-  })
   $("[data-action=clear]").click(function () {
-    $("[data-action=clear]").removeClass('open')
-  })
+    $("[data-action=clear]").removeClass("open");
+  });
+  $("[data-action=today]").click(function () {
+    $(".filter-date").datepicker().data("datepicker").hide();
+    $(".air-datepicker").removeClass("active");
+  });
 
-}())
+  $(".dropdown-date__range_from").datepicker({
+    onShow: function (dp, animationCompleted) {
+      if (!animationCompleted) {
+        $(".js-dropdown-date-range-from").addClass("active");
+      }
+    },
+    onHide: function (dp, animationCompleted) {
+      if (!animationCompleted) {
+        $(".js-dropdown-date-range-from").removeClass("active");
+      }
+    },
+    minDate: new Date(),
+    clearButton: true,
+    todayButton: true,
+    language: {
+      today: "Применить",
+    },
+  });
+  $(".datepicker--cells").click(function () {
+    $("[data-action=clear]").addClass("open");
+  });
+  $("[data-action=clear]").click(function () {
+    $("[data-action=clear]").removeClass("open");
+  });
+  $("[data-action=today]").click(function () {
+    $(".dropdown-date__range_from").datepicker().data("datepicker").hide();
+    $(".js-dropdown-date-range-from").removeClass("active");
+  });
 
+  $(".dropdown-date__range_to").datepicker({
+    onShow: function (dp, animationCompleted) {
+      if (!animationCompleted) {
+        $(".js-dropdown-date-range-to").addClass("active");
+      }
+    },
+    onHide: function (dp, animationCompleted) {
+      if (!animationCompleted) {
+        $(".js-dropdown-date-range-to").removeClass("active");
+      }
+    },
+    minDate: new Date(),
+    clearButton: true,
+    todayButton: true,
+    language: {
+      today: "Применить",
+    },
+  });
+  $(".datepicker--cells").click(function () {
+    $("[data-action=clear]").addClass("open");
+  });
+  $("[data-action=clear]").click(function () {
+    $("[data-action=clear]").removeClass("open");
+  });
+  $("[data-action=today]").click(function () {
+    $(".dropdown-date__range_to").datepicker().data("datepicker").hide();
+    $(".js-dropdown-date-range-to").removeClass("active");
+  });
 
+  $(".datepicker-here").datepicker({
+    minDate: new Date(),
+    clearButton: true,
+    todayButton: true,
+    language: {
+      today: "Применить",
+    },
+  });
+});
