78cc4ad2de7868e26ac7444f65a18b64bf8e017a
Ramil <ram_ram_14@list.ru>
2021-10-23 22:23:48 +0300

    updated DropdownGuest

--- src/components/guest-dropdown/drop-guest.js
+++ src/components/guest-dropdown/drop-guest.js
@@ -4,16 +4,21 @@ class DropdownGuest {
   constructor(el) {
     this.open = this.open.bind(this);
     this.close = this.close.bind(this);
-    this.hide = this.hide.bind(this);
     this.render(el);
   }
 
   render(el) {
     this.dropDownBtn = el.querySelector(".dropdown__button");
     this.dropDownList = el.querySelector(".dropdown__list");
-    this.btnsGuest = el.querySelectorAll(".counter__btn-guest");
     this.btnApply = el.querySelector(".apply");
-    this.btnClear = el.querySelector(".clear");
+    this.dropdownguest = document.querySelector(".dropdown-guest");
+    this.btnsGuest = document.querySelectorAll(".counter__btn-guest");
+    this.btnClear = document.querySelector(".clear");
+    this.btnApply = document.querySelector(".apply");
+    this.input = document.querySelector(".dropdown__input-guest");
+    this.inputs = document.querySelectorAll(".guest__counter");
+    this.list = document.querySelector(".dropdown__list-item");
+    this.counter();
     this.addEventListeners();
   }
 
@@ -28,23 +33,95 @@ class DropdownGuest {
     this.dropDownBtn.classList.remove("dropdown__button_active");
   }
 
-  hide(e) {
-    const itsMenu =
-      e.target == this.dropDownList || this.dropDownList.contains(e.target);
-    const itsBtnMenu = e.target == this.dropDownBtn;
-    const menuIsActive = this.dropDownList.classList.contains(
-      "dropdown__list_active"
-    );
-
-    if (!itsMenu && !itsBtnMenu && menuIsActive) {
-      this.close();
-    }
-  }
-
   addEventListeners() {
     this.dropDownBtn.addEventListener("click", this.open);
     this.btnApply.addEventListener("click", this.close);
-    document.addEventListener("click", this.hide);
+    this.dropDownList.addEventListener("click", (e) => e.stopPropagation());
+    document.addEventListener("click", this.close);
+  }
+
+  counter() {
+    if (this.dropdownguest) {
+      this.btnsGuest.forEach(
+        function (btn) {
+          const minBtn = btn.parentElement.querySelector(
+            "[data-direction='minus']"
+          );
+          const inpGuest = btn.parentElement.querySelector(".guest__counter");
+
+          btn.addEventListener(
+            "click",
+            function () {
+              const direction = btn.dataset.direction;
+
+              const currentValue = +inpGuest.value;
+              let newValue;
+
+              if (direction === "minus") {
+                newValue = currentValue - 1 > 0 ? currentValue - 1 : 0;
+              } else if (direction === "plus") {
+                newValue = currentValue + 1 < 9 ? currentValue + 1 : 9;
+              }
+
+              inpGuest.value = newValue;
+
+              let sum = 0;
+              this.inputs.forEach(function (i) {
+                sum += parseInt(i.value);
+              });
+
+              this.btnClear.addEventListener(
+                "click",
+                function () {
+                  inpGuest.value = 0;
+                  this.input.value = "Сколько гостей";
+                  sum = 0;
+                  this.btnClear.classList.remove("active");
+                  minBtn.classList.remove("active");
+                }.bind(this)
+              );
+
+              this.clear(inpGuest.value, minBtn);
+              this.clear(sum, this.btnClear);
+
+              this.btnApply.addEventListener(
+                "click",
+                function (e) {
+                  this.input.value = `${sum} ${getCounterValue(
+                    sum,
+                    this.list.dataset.value
+                  )}`;
+
+                  this.showValue();
+                }.bind(this)
+              );
+            }.bind(this)
+          );
+        }.bind(this)
+      );
+    }
+  }
+
+  showValue() {
+    let list = [...this.dropdownguest.querySelectorAll(".guest__counter_baby")];
+
+    list = list.map((el) => {
+      const word = getCounterValue(el.value, el.dataset.value);
+      return `${el.value} ${word}`;
+    });
+
+    if (list > "1") {
+      this.dropdownguest.querySelector(".guest__input").value =
+        this.input.value + ", " + list.join(", ");
+    }
+  }
+
+  clear(sum, btn) {
+    if (sum === 0 || sum < 1) {
+      btn.classList.remove("active");
+    } else {
+      btn.classList.add("active");
+    }
   }
 }
 
--- src/js/main.js
+++ src/js/main.js
@@ -33,7 +33,6 @@ import "../components/submenu/submenu";
 import "../components/dropdown/dropdown.js";
 import "../components/guest-dropdown/drop-guest.js";
 import "../components/room/rooms.js";
-import "../components/guest-dropdown/guest-counter.js";
 import "../components/dropdown-checkbox/dropdown-checkbox.js";
 import "../components/air-datepicker/air-datepicker.js";
 
