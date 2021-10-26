d1752c79f59674c343cac789226eb7c66e859908
Ramil <ram_ram_14@list.ru>
2021-10-26 15:31:06 +0300

    убрал неиспользуемые переменные, одинарные кавычки

--- src/components/swiper/swiper.js
+++ src/components/swiper/swiper.js
@@ -1,31 +1,29 @@
-import Swiper from 'swiper';
-import SwiperCore, { Navigation, Pagination } from 'swiper/core';
+import Swiper from "swiper";
+import SwiperCore, { Navigation, Pagination } from "swiper/core";
 SwiperCore.use([Navigation, Pagination]);
 (function () {
-
-  const swiper = new Swiper('.swiper-container', {
+  new Swiper(".swiper-container", {
     slidesPerView: 1,
     spaceBetween: 0,
     loop: true,
     pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
+      el: ".swiper-pagination",
+      type: "bullets",
       clickable: true,
     },
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
   });
-  const swiper2 = new Swiper('.swiper-container2', {
+  new Swiper(".swiper-container2", {
     slidesPerView: 1,
     spaceBetween: 0,
     loop: true,
     pagination: {
-      el: '.swiper-pagination2',
-      type: 'bullets',
+      el: ".swiper-pagination2",
+      type: "bullets",
       clickable: true,
     },
   });
-
-}())
+})();
